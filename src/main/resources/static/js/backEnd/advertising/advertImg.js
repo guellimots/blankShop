544ea0eea8ec54a18adeef081cb20dab84ad9b08
allenklinghoffer <53673169+allenklinghoffer@users.noreply.github.com>
2021-05-12 22:35:39 +0800

    移除多餘顯示

--- src/main/resources/static/js/backEnd/advertising/advertImg.js
+++ src/main/resources/static/js/backEnd/advertising/advertImg.js
@@ -8,10 +8,13 @@ $(document).ready(function(){
 	function check(){
 		re = $("#resultmsg").html();
 		if(re == "上傳成功"){
-			swal("新增成功","","success",{button:"確定"});
+			swal("新增成功","","success",{button:"確定"}).then(function(){
+				$('#resultmsg').css('display','none')
+			});
 		}
 		if(re == "上傳失敗"){
-			swal("新增失敗","","error",{button:"確定"});
+			swal("新增失敗","","error",{button:"確定"}).then(function(){
+				$('#resultmsg').css('display','none');
 		}
 	}
 })
